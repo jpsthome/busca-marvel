33a6f4c6b5710de234b4cb830bf395537226debd
Joao Pedro <joao.thome@unila.edu.br>
2021-09-18 13:03:20 -0300

    atualizando arquivos de enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'http://gateway.marvel.com//v1/public/',
+  apiPublicKey: '<Your public API key here>',
+  apiPrivateKey: '<Your private API key here>',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiUrl: 'http://gateway.marvel.com//v1/public/',
+  apiPublicKey: '<Your public API key here>',
+  apiPrivateKey: '<Your private API key here>',
 };
 
 /*
