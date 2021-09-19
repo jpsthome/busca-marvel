a6872e6136645bb7202f4c8e13f453ce15269e21
Joao Pedro <joao.thome@unila.edu.br>
2021-09-19 18:47:56 -0300

    adicionando loading component

--- src/app/characters/characters.module.ts
+++ src/app/characters/characters.module.ts
@@ -1,11 +1,10 @@
+import { SharedModule } from './../shared/shared.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CharactersListComponent } from './components/characters-list/characters-list.component';
 
 @NgModule({
-  declarations: [
-    CharactersListComponent
-  ],
-  imports: [CommonModule],
+  declarations: [CharactersListComponent],
+  imports: [CommonModule, SharedModule],
 })
 export class CharactersModule {}
