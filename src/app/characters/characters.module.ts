80e131bde4cc54a2594dfc10b626909f835e6b83
Joao Pedro <joao.thome@unila.edu.br>
2021-09-19 18:48:56 -0300

    adicionando character-row component

--- src/app/characters/characters.module.ts
+++ src/app/characters/characters.module.ts
@@ -2,9 +2,10 @@ import { SharedModule } from './../shared/shared.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CharactersListComponent } from './components/characters-list/characters-list.component';
+import { CharacterRowComponent } from './components/characters-list/character-row/character-row.component';
 
 @NgModule({
-  declarations: [CharactersListComponent],
+  declarations: [CharactersListComponent, CharacterRowComponent],
   imports: [CommonModule, SharedModule],
 })
 export class CharactersModule {}
