222cb7d3c747492d34c2b87bda5e76eeb53ef6d2
Joao Pedro <joao.thome@unila.edu.br>
2021-09-20 02:14:07 -0300

    adicionando paginação à tabela de usuários

--- src/app/characters/components/characters-list/characters-list.component.ts
+++ src/app/characters/components/characters-list/characters-list.component.ts
@@ -11,6 +11,9 @@ export class CharactersListComponent implements OnInit {
   characters: Character[] = [];
   charactersName: string = '';
   loading = true;
+  pages: number = 0;
+  currentPage: number = 0;
+  lastPage: number = 5;
   constructor(private characterService: CharactersService) {}
 
   ngOnInit(): void {
@@ -20,8 +23,10 @@ export class CharactersListComponent implements OnInit {
   getCharacters() {
     this.characterService.getCharacters().subscribe((res) => {
       this.loading = false;
-      this.characters = res;
+      this.characters = res.results;
       console.log(this.characters);
+      this.pages = Math.floor(res.total / 10);
+      console.log(this.pages);
     });
   }
 
@@ -31,17 +36,50 @@ export class CharactersListComponent implements OnInit {
     });
   }
 
+  pageCounter(i: number) {
+    console.log(this.currentPage);
+    const array = [
+      this.currentPage,
+      this.currentPage + 1,
+      this.currentPage + 2,
+      this.currentPage + 3,
+      this.currentPage + 4,
+      this.currentPage + 5,
+    ];
+    console.log(array);
+    return array;
+  }
+
+  getCharactersPage(page: number) {
+    this.currentPage = page;
+    this.lastPage = page + 5;
+    this.loading = true;
+    if (this.charactersName) {
+      this.characterService
+        .getCharactersByName(this.charactersName, page)
+        .subscribe((res) => {
+          this.characters = res.results;
+          this.loading = false;
+        });
+    } else {
+      this.characterService.getCharacters(page).subscribe((res) => {
+        this.characters = res.results;
+        console.log(res);
+        this.loading = false;
+      });
+    }
+  }
   updateCharactersName(e: string) {
     this.charactersName = e;
     this.loading = true;
     if (e) {
       this.characterService.getCharactersByName(e).subscribe((res) => {
-        this.characters = res;
+        this.characters = res.results;
         this.loading = false;
       });
     } else {
       this.characterService.getCharacters().subscribe((res) => {
-        this.characters = res;
+        this.characters = res.results;
         this.loading = false;
       });
     }
